086efcc33c4569b6e0f6f224710cd2da2e8c68bd
Abhinay Agarwal <abhinay.agarwal@gluonhq.com>
2019-07-08 17:23:53 +0530

    Rename file and use maven to find project version

--- .ci/deploy-snapshot.sh
+++ .ci/deploy-snapshot.sh
@@ -1,13 +1,11 @@
 #!/usr/bin/env bash
 
 # Find version
-ver=$(grep -m1 '<version>' pom.xml)
-ver=${ver%<*};
-ver=${ver#*>};
+ver=$(mvn help:evaluate -Dexpression=project.version -q -DforceStdout)
 
 # deploy if snapshot found
 if [[ $ver == *"SNAPSHOT"* ]] 
 then
     cp .travis.settings.xml $HOME/.m2/settings.xml
     mvn deploy -DskipTests=true -Dgpg.skip
-fi 
+fi
